29c40c3d69ca0569f135037cf078bf59c9480bc3
Pedro Molina <pedro.molina@petroprix.com>
2022-09-20 21:02:14 +0200

    clase 99 y 100 completadas

--- POO/Leccion01/Rectangulo.py
+++ POO/Leccion01/Rectangulo.py
@@ -3,9 +3,11 @@ class Rectangulo:
         self.altura = altura
         self.base = base
 
-    def calcularArea(self):
+    def calcular_area(self):
         return self.altura * self.base
 
 
-rectangulo1 = Rectangulo(3,7)
-print(f'El área del rectángulo de altura: {rectangulo1.altura} y de base {rectangulo1.base} es igual a: {rectangulo1.calcularArea()}')
+base = int(input("Proporciona la base: "))
+altura = int(input("Proporciona la altura: "))
+rectangulo1 = Rectangulo(base,altura)
+print(f'El área del rectángulo de altura: {rectangulo1.altura} y de base {rectangulo1.base} es igual a: {rectangulo1.calcular_area()}')
