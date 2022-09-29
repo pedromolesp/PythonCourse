c38a0141890f2a90b2ccd9e6bf6d011960118d8a
Pedro Molina <pedro.molina@petroprix.com>
2022-09-29 23:44:42 +0200

    clase 105 terminada

--- POO/Leccion01/Persona.py
+++ POO/Leccion01/Persona.py
@@ -1,24 +1,37 @@
 class Persona:
     def __init__(self, nombre, apellido, edad, *valores , **terminos):
-        self.nombre = nombre
+        self._nombre = nombre
         self.apellido = apellido
         self.edad = edad
         self.valore = valores
         self.terminos = terminos
 
+    @property
+    def nombre(self):
+        return self._nombre
+    @nombre.setter
+    def nombre(self, nombre):
+        print("llamando método set nombre"+nombre)
+
+        self._nombre = nombre
+
     def mostrar_detalle(self):
-        print(f'Persona: {self.nombre} {self.apellido} {self.edad} {self.valore} {self.terminos}')
+        print(f'Persona: {self.nombre} {self.apellido} {self.edad}')
 
 persona1 = Persona("Pedro","Molina",27)
-persona1.mostrar_detalle()
-persona1.telefono = '664612349'
-print(persona1.telefono)
+print(persona1.nombre)
+persona1.nombre = "Brujo"
+print(persona1.nombre)
+
+# persona1.mostrar_detalle()
+# persona1.telefono = '664612349'
+# print(persona1.telefono)
 # print(f'Objeto persona 1: {persona1.nombre} {persona1.apellido} {persona1.edad}')
 
 # Persona.mostrar_detalle(pers ona1)
 
-persona2 = Persona("Fernando","Cuadros",24)
-persona2.mostrar_detalle()
-print(persona2.telefono)
+# persona2 = Persona("Fernando","Cuadros",24)
+# persona2.mostrar_detalle()
+# print(persona2.telefono)
 
 # print(f'Objeto persona 2: {persona2.nombre} {persona2.apellido} {persona2.edad}')
